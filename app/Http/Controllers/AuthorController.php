267cb0b2ba6ff4c81fb99ede086b3854c740ad27
Orihama-kajiki <kajiki.business@gmail.com>
2022-12-08 19:25:03 +0900

    first-commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+  public function index()
+  {
+    $authors = Author::all();
+    return view('index', ['authors' => $authors]);
+  }
+  // 追記：ここから
+  public function find()
+  {
+    return view('find', ['input' => '']);
+  }
+
+  public function search(Request $request)
+  {
+    $author = Author::find($request->input);
+    $param = [
+      'author' => $author,
+      'input' => $request->input
+    ];
+    return view('find', $param);
+  }
+  // 追記：ここまで
+}
